1574038859eda1f9deb34a8fc12064ca5977f171
Nils E <nerognerud@gmail.com>
2017-03-08 20:18:37 -0800

    test push

---
+++ cmake-build-debug/CMakeFiles/simpleLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpleLinkedList.dir/main.cpp.o"
+  "simpleLinkedList.pdb"
+  "simpleLinkedList.exe"
+  "libsimpleLinkedList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simpleLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
